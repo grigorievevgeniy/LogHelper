039b9f6e1c3931f6a771d5b2904ad3f0a4515021
Evgeny.Grigoryev <timeinvest27@gmail.com>
2021-09-13 12:31:57 +0300

    Add project files.

---
+++ LogHelper/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace LogHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLogValue = new System.Windows.Forms.Button();
+            this.btnLogName = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(12, 12);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(151, 23);
+            this.btnEnter.TabIndex = 0;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 181);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(151, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLogValue
+            // 
+            this.btnLogValue.Location = new System.Drawing.Point(12, 52);
+            this.btnLogValue.Name = "btnLogValue";
+            this.btnLogValue.Size = new System.Drawing.Size(151, 50);
+            this.btnLogValue.TabIndex = 2;
+            this.btnLogValue.Text = "Logger.Error($\"x - {x}\")";
+            this.btnLogValue.UseVisualStyleBackColor = true;
+            this.btnLogValue.Click += new System.EventHandler(this.btnLogValue_Click);
+            // 
+            // btnLogName
+            // 
+            this.btnLogName.Location = new System.Drawing.Point(12, 108);
+            this.btnLogName.Name = "btnLogName";
+            this.btnLogName.Size = new System.Drawing.Size(151, 50);
+            this.btnLogName.TabIndex = 3;
+            this.btnLogName.Text = "Logger.Error(\"x\")";
+            this.btnLogName.UseVisualStyleBackColor = true;
+            this.btnLogName.Click += new System.EventHandler(this.btnLogName_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(175, 212);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnLogName);
+            this.Controls.Add(this.btnLogValue);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEnter);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Log Help";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLogValue;
+        private System.Windows.Forms.Button btnLogName;
+    }
+}
+
---
+++ LogHelper/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Logger.Error(\"Enter\");");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("Logger.Error(\"Exit\");");
+        }
+
+        private void btnLogValue_Click(object sender, EventArgs e)
+        {
+            // ToDo Проверка что в буфере текст.
+
+            var text = Clipboard.GetText();
+            Clipboard.SetText($"Logger.Error($\"{text} - {{{text}}}\");");
+        }
+
+        private void btnLogName_Click(object sender, EventArgs e)
+        {
+            // ToDo Проверка что в буфере текст.
+            
+            var text = Clipboard.GetText();
+            Clipboard.SetText($"Logger.Error(\"{text}\");");
+        }
+    }
+}
